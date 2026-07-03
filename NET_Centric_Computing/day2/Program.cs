ac0e97be0a7a07a7cc1fea10ad9b92faea5461c9
Abinash Regmi <abinashregmi01234@gmail.com>
2026-07-03 15:30:40 +0545

    change

---
+++ NET_Centric_Computing/day2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace day2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var x = 18;
+            if (x >= 18)
+            {
+                Console.WriteLine("You are an adult obivously.");
+            }
+            else
+            {
+                Console.WriteLine("You are a minor.");
+            }
+        }
+    }
+}
