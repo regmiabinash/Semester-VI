eaf29177ec7a3068c0e84ffa6bfc359c97caad1c
Abinash Regmi <abinashregmi01234@gmail.com>
2026-07-03 15:38:38 +0545

    change

---
+++ NET_Centric_Computing/day2part2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace day2part2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string gender = "F";
+            int Age = 17;
+            if (gender== "F")
+            {
+                Console.WriteLine("You are a female");
+                 if (Age < 18)
+                {
+                    Console.WriteLine("You are less than 18");
+                }
+                else
+                {
+                    Console.WriteLine("Age is greater than 18");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("You are a male");
+                if (Age < 18)
+                {
+                    Console.WriteLine("You are less than 18");
+
+                }
+                else
+                {
+                    Console.WriteLine("Age is greater than 18");
+                }
+            }
+        }
+    }
+}
