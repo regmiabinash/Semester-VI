c5ceba1cf8892d8045b8dab12a270e8d854c5cc9
Abinash Regmi <abinashregmi01234@gmail.com>
2026-07-06 14:16:08 +0545

    change

---
+++ NET_Centric_Computing/day3jump/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace jump
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Each Loop");
+            int[] numbersArray = { 1, 2, 3, 4, 5 };
+            foreach (int number in numbersArray)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ NET_Centric_Computing/dowhile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Dowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            while (a < 10)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+
+            int b = 2;
+            do
+            {
+                Console.WriteLine(b);
+                b++;
+            }
+            while (b < 10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
