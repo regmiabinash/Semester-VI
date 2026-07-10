35af88091ba91ea36758e5370c276b72b505ecb0
Abinash Regmi <abinashregmi01234@gmail.com>
2026-07-10 11:04:18 +0545

    array

---
+++ NET_Centric_Computing/array/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace array
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            for (int i =0; i<numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Even number:");
+            foreach (int number in numbers)
+            {
+                if (number%2 == 0)
+                {
+                    Console.WriteLine(" The even numbers are :" + number);
+                }
+                
+            }
+
+            Console.WriteLine("Odd number:  ");
+            foreach(int number in numbers)
+            {
+                if (number % 2 != 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            //string[] arr = new string[4];
+            //Console.WriteLine("Enter 4 Names: ");
+            
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i]=Console.ReadLine();
+   
+            //}
+            //foreach (string name in arr)
+            //{
+
+
+                
+            //    Console.WriteLine(name);
+            //}
+
+        }
+    }
+}
