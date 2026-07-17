18690739fbc6650ded9eff9f72b1e89a7c0813d7
Abinash Regmi <abinashregmi01234@gmail.com>
2026-07-17 10:57:48 +0545

    learning constructor

---
+++ NET_Centric_Computing/fridayclass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace constructor
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+
+
+        public Person()
+        {
+             Name = "Unknown";
+             Age = 22;
+        }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+        }
+
+        public Person(Person person)
+        {
+            Name = person.Name;
+            Age = person.Age;
+        }
+
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.DisplayInfo();
+            Person person2 = new Person("Alice", 30);
+            person2.DisplayInfo();
+            Person person3 = new Person(person2);
+            person3.DisplayInfo();
+        }
+    }   
+}
