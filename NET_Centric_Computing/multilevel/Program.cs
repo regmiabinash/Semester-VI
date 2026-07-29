6f499259cba0ab47c8f33bdcad33faa2fd9f7512
Abinash Regmi <abinashregmi01234@gmail.com>
2026-07-29 22:24:48 +0545

    nothing

---
+++ NET_Centric_Computing/multilevel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Multilevel
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public void Display()
+        {
+            Console.WriteLine($"Name: {Name}");
+        }
+    }
+    public class Domestic : Animal
+    {
+    }
+    public class Cat : Domestic
+    {
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal A = new Animal();
+            A.Name = "Animal";
+            A.Display();
+            Domestic D = new Domestic();
+            D.Name = "Domestic";
+            D.Display();
+            Cat C = new Cat();
+            C.Name = "Cat";
+            C.Display();
+        }
+    }
+}
