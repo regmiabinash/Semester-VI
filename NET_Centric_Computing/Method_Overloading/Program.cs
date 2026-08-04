d2de4e253b33ca50483140374801df192e559598
Abinash Regmi <abinashregmi01234@gmail.com>
2026-08-04 17:52:23 +0545

    sometype of inheritance

---
+++ NET_Centric_Computing/Method_Overloading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Method_Overloading
+{
+    public class Student
+    {
+        public String Info(int ID, string Name)
+        {
+            Console.WriteLine("Student ID: " + ID);
+            Console.WriteLine("Student Name: " + Name);
+            return "Student Info: " + ID + ", " + Name;
+        }
+        public String Info(string fname, string lname)
+        {
+            Console.WriteLine("Student First Name: " + fname);
+            Console.WriteLine("Student Last Name: " + lname);
+            return "Student Name: " + fname + " " + lname;
+        }
+        public String Info(int ID, string Name, int Rollno)
+        {
+            Console.WriteLine("Student ID: " + ID);
+            Console.WriteLine("Student Name: " + Name);
+            Console.WriteLine("Student Roll No: " + Rollno);
+            return "Student Info: " + ID + ", " + Name + ", " + Rollno;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Info(1, "Dipesh");
+            student.Info("Dipesh", "Bista");
+            student.Info(7, "Dipesh", 18);
+        }
+    }
+}
---
+++ NET_Centric_Computing/absentuesday/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MethodOverriding
+{
+    public class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The animal makes a sound.");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The dog barks.");
+        }
+    }
+    public class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The cat meows.");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            myAnimal.Speak();
+            Animal myDog = new Dog();
+            myDog.Speak();
+            Animal myCat = new Cat();
+            myCat.Speak();
+        }
+    }
+}
