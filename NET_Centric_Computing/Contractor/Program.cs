00fe7606cbcc366107b6c95dfb07c2b64da729e5
Abinash Regmi <abinashregmi01234@gmail.com>
2026-08-06 10:57:25 +0545

    learning delegate

---
+++ NET_Centric_Computing/Contractor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ThakeyDar
+{
+    public class NewHouse
+    {        //static void Main(string[] args)
+
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+        public delegate string Thakeydar();
+        public static string Labour()
+        {
+            Console.WriteLine("This is me Labour");
+            return "This is me Labour";
+        }
+        public static string Painter()
+        {
+            Console.WriteLine("This is me Austrian Painter");
+            return "This    ";
+
+        }
+        public static string Marvel()
+        {
+            Console.WriteLine("This is me Hitler");
+            return "This";
+        }
+
+        public static void Main()
+        {
+            Thakeydar del = Labour;
+            del();
+
+            del = Painter;
+            del();
+
+            del = Marvel;
+            del();
+
+        }
+    }
+}
---
+++ NET_Centric_Computing/DelhiGate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public delegate void MyDelegate();
+
+
+    public static void Hello()
+    {
+        Console.WriteLine("Hello World! ");
+    }
+
+    public static void HiFunc()
+    {
+        Console.WriteLine("Hi World! ");
+    }
+
+    public static void Main()
+
+    {
+        MyDelegate del = Hello;
+        del();
+        del = HiFunc;
+        del();
+    }
+
+
+}
+
+
